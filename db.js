e5d6e80c14648d6fc1370f9b10e8052399f981d3
caglaror <caglaror@gmail.com>
2019-05-20 15:18:05 -0700

    db, app

--- db.js
+++ db.js
@@ -1,3 +1,15 @@
 const mongoose = require("mongoose");
-mongoose.connect('mongodb://127.0.0.1:27017/user-list',{useMongoClient:true});
+mongoose.connect('mongodb://127.0.0.1:27017/user-list',{useNewUrlParser: true });
+
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+    user_name: String,
+    user_lastname: String,
+    user_dob: String,
+    user_email:String
+});
+
+mongoose.model("User",userSchema);
+
+module.exports = mongoose;
 
